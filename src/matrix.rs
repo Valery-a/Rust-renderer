477fff28eeb9a7ff8af0f07bd6e8898fbda6303a
Valery-a <ValeryKulchicki@gmail.com>
2023-09-05 13:51:58 +0300

    Implement Display for Mat and add coords_impl! macro

--- src/matrix.rs
+++ src/matrix.rs
@@ -27,3 +27,31 @@ impl<T : Value, N : Clone + Unsigned, M : Clone + Unsigned> Copy for Mat<T, N, M
     GenericArray<T, typenum::Prod<N,M>> : Copy{
 
 }
+
+
+impl<T: Value + Display, N: Unsigned + Clone, M: Unsigned + Clone> Display for Mat<T, N, M>
+where
+    N: Mul<M>,
+    Prod<N, M>: ArrayLength<T>,
+{
+    fn fmt(&self, f: &mut Formatter) -> Result<(), fmt::Error> {
+        for i in 0..N::to_usize() {
+            write!(f, "[ ")?;
+            for j in 0..M::to_usize() {
+                write!(f, "{:10.4} ", self[(i, j)])?;
+            }
+            writeln!(f, "]")?;
+        }
+        Ok(())
+    }
+}
+
+macro_rules! coords_impl(
+    ($T: ident; $($comps: ident),*) => {
+        #[repr(C)]
+        #[derive(Eq, PartialEq, Clone, Hash, Debug, Copy)]
+        pub struct $T<N : Value> {
+            $(pub $comps: N),*
+        }
+    }
+);
