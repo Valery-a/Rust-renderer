6ef85e880eb79397044fbcf08c9a547b0ae655a3
Valery-a <ValeryKulchicki@gmail.com>
2023-09-05 09:04:54 +0300

    added base for mesh renderer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,44 @@
-use bomba::run;
+extern crate glad_gl;
+extern crate glad_vulkan;
+#[cfg(not(target_os = "macos"))]
+extern crate openblas_src;
 
-fn main() {
-    pollster::block_on(run());
+#[cfg(feature = "vulkan")]
+extern crate vulkano;
+#[cfg(feature = "vulkan")]
+extern crate vulkano_shaders;
+#[cfg(feature = "vulkan")]
+mod vulkan_raytracer;
+
+use glad_gl::gl::GL_TEXTURE_2D;
+use std::vec::Vec as Vector;
+
+mod graphics;
+mod graphics_util;
+
+#[macro_use]
+mod matrix;
+
+//#[macro_use]
+//mod matrix_const;
+
+mod extraction;
+
+use extraction::uniform_manifold_dc::*;
+//use extraction::adaptive_manifold_dc;
+
+use noise::Perlin;
+use graphics::*;
+use renderer::*;
+use math::*;
+use matrix::*;
+use glad_gl::gl;
+
+
+fn run(){
+    
+}
+
+fn main(){
+    run();
 }
