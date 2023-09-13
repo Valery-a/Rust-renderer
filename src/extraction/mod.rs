4bbb57a08d7f8cf11c57d4d42935dd7df7bc9820
Valery-a <ValeryKulchicki@gmail.com>
2023-09-13 19:06:22 +0300

    refferencing map and finishing off main.rs

---
+++ src/extraction/mod.rs
@@ -0,0 +1,1 @@
+pub mod uniform_manifold_dc;
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+extern crate generic_array;
+extern crate typenum;
+extern crate alga;
+extern crate libc;
+extern crate ansi_term;
+extern crate time;
+extern crate rand;
+extern crate noise;
+extern crate glfw;
 extern crate glad_gl;
 extern crate glad_vulkan;
+#[cfg(target_os = "macos")]
+extern crate accelerate_src;
 #[cfg(not(target_os = "macos"))]
 extern crate openblas_src;
+extern crate lapack_sys;
 
 #[cfg(feature = "vulkan")]
 extern crate vulkano;
@@ -15,7 +27,9 @@ use std::vec::Vec as Vector;
 
 mod graphics;
 mod graphics_util;
+mod renderer;
 
+mod math;
 #[macro_use]
 mod matrix;
 
@@ -118,7 +132,79 @@ fn handle_input(_glfw: &mut glfw::Glfw, win: &mut glfw::Window, dt_ns: u64, came
 
 
 fn run(){
-    
+    let def_width: u32 = 800;
+    let def_height: u32 = 600;
+    let title = "bomba";
+    let cam = renderer::Cam{look : Vec3::new(0.0, 0.0, -1.0), up : Vec3::new(0.0, 1.0, 0.0), pos : Vec3::new(0.0, 0.0, 0.0)};
+    let mut renderer = Renderer::new(cam);
+    renderer.init(def_width, def_height, title);
+    renderer.set_framebuffer_size_callback(|w, h| println!("new dims {} {}", w, h));
+
+
+    let _block_size : f32 = 0.125;//2.0;
+    let _chunk_size : usize = 128;//*2;
+
+
+
+    //UNIFORM MANIFOLD DC
+    let perlin = Perlin::new();
+    let noise = noise_f32(perlin, Cube{center : Vec3::new(1.0,-1.0,1.0), extent : 3.5} );//perlin.get([p.x,p.y,p.z])  ;
+    //let den4 = union3(den3, mk_half_space_pos(Plane{point : Vec3::new(0.0, 2.0, -4.0), normal : Vec3::new(1.0, 1.0, 0.0).normalize()}));
+    //let den = f;
+    //construct_grid(&den4, Vec3::new(-3.0, -3.0, -8.0), BLOCK_SIZE, CHUNK_SIZE, 8, &mut renderer_tr_light, &mut renderer_lines);
+
+    let test_sphere = Sphere{center : Vec3::new(2.7, -1.0, 0.0), rad : 0.1};
+    let test_sphere2 = Sphere{center : Vec3::new(2.7, 3.0, 0.0), rad : 0.1};
+    let test_sphere3 = Sphere{center : Vec3::new(2.7, 1.0, 2.7), rad : 0.1};
+    let ts1 = mk_sphere(test_sphere);
+    let ts2 = mk_sphere(test_sphere2);
+    let ts22 = mk_sphere(test_sphere3);
+    let ts3 = difference3(ts1, ts2);
+    let _ts4 = difference3(ts3, ts22);
+    let a1 = union3(difference3(noise, ts1), mk_obb(Vec3::new(-1.0, -1.0, 0.0), Vec3::new(1.0, -1.0, 0.0).normalize(), Vec3::new(1.0, 1.0, 0.5).normalize(), Vec3::new(1.0, 0.5, 0.2)));
+    //add_sphere_color(&mut renderer_tr_light, &test_sphere, 100, 100, Vec3::new(1.0, 1.0, 1.0));
+    //construct_grid(&ts4, Vec3::new(-0.5, -2.5, -2.5), 1.0/8.0, 2*8*8, 32, &mut renderer.render_triangles_lighting_pos_color_normal, &mut renderer.render_lines_pos_color);
+    let mut triangles_for_rt = Vector::with_capacity(1000);
+    construct_grid(a1, Vec3::new(-4.0, -2.5, -4.5), 0.125/2.0, 128, 16, &mut renderer.render_triangles_lighting_pos_color_normal, &mut renderer.render_lines_pos_color, &mut triangles_for_rt);
+    //unsafe { uni_manifold_dc::sample_grid(a1, Vec3::new(-4.0, -2.5, -4.5), 0.125 / 2.0, 128, 16, 0.001, &mut renderer.render_lines_pos_color); }
+
+    //triangle / mesh
+    //add_triangle_color(&mut renderer.render_triangles_pos_color, Triangle3{p1 : vec3![-0.2, 0.0, -1.0], p2 : vec3![0.2, 0.0, -1.0], p3 : vec3![0.0, 0.3, -1.0]}, red);
+    //add_grid3_pos_color(&mut renderer.render_lines_pos_color, Vec3::new(0.0, 0.0, 0.0), Vec3::new(1.0, 0.0, 0.0), Vec3::new(0.0, 1.0, 0.0), 1.0, 8, white);
+
+    //println!("{:?}", img);
+    /*let mut tex = [0u32];
+    gl_active_texture(gl::GL_TEXTURE0);
+    gl_gen_textures(1, &mut tex);
+    gl_bind_texture(gl::GL_TEXTURE_2D, tex[0]);
+    gl_tex_parameteri(gl::GL_TEXTURE_2D, gl::GL_TEXTURE_MAG_FILTER, gl::GL_LINEAR);
+    gl_tex_parameteri(gl::GL_TEXTURE_2D, gl::GL_TEXTURE_MIN_FILTER, gl::GL_LINEAR);
+    renderer.render_triangles_texture_screen_pos_tex.data = tex[0];*/
+    /*gl_use_program(rs_prog);*/
+    //gl_dispatch_compute(def_width, def_height, 1);
+    //gl_memory_barrier(gl::GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+
+    //vulkan_raytracer::setup(def_width, def_height, &cam, &triangles_for_rt);
+    gl_generate_mipmap(GL_TEXTURE_2D);
+
+
+
+    //add_quad_pos_tex(&mut renderer.render_triangles_texture_screen_pos_tex, [vec3![0.0, 0.0, 0.0], vec3![800.0, 0.0, 0.0], vec3![800.0, 600.0, 0.0], vec3![0.0, 600.0, 0.0]], [vec2![0.0, 1.0], vec2![1.0, 1.0], vec2![1.0, 0.0], vec2![0.0, 0.0]]);
+
+
+    renderer.run(move |renderer, dt_ns| {
+
+        handle_input(renderer.glfw.as_mut().unwrap(), renderer.window.as_mut().unwrap(), dt_ns, &mut renderer.camera);
+
+        //let img = vulkan_raytracer::setup(def_width, def_height, &renderer.camera, &triangles_for_rt);
+        //gl_tex_image_2d(gl::GL_TEXTURE_2D, 0, gl::GL_RGBA, def_width, def_height, 0, gl::GL_RGBA, gl::GL_UNSIGNED_BYTE, img.as_slice());
+
+        gl_enable(gl::GL_DEPTH_TEST);
+        gl_clear(gl::GL_COLOR_BUFFER_BIT | gl::GL_DEPTH_BUFFER_BIT);
+        gl_clear_color(0.3, 0.2, 0.6, 1.0);
+
+        
+    });
 }
 
 fn main(){
